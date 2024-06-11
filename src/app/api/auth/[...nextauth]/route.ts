2a892c4cb90b56e72e45857a84b2042ca734539f
Guru <47211091+GuruIT13@users.noreply.github.com>
2024-06-11 17:19:39 +0700

    commit

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,100 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { NextResponse } from "next/server";
+import { sqlConnect } from "../../../../../public/components/lib/db";
+import * as sql from "mssql";
+
+const handler = NextAuth({
+  session: {
+    strategy: "jwt",
+    maxAge: 1 * 24 * 60 * 60,
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req): Promise<any> {
+        try {
+          if (!credentials?.username || !credentials?.password) {
+            throw new Error("Credentials not provided");
+          }
+          let user = null;
+          const pool = await sqlConnect();
+          const users = await pool
+            .request()
+            .input("username", sql.VarChar, credentials?.username)
+            .input("password", sql.VarChar, credentials?.password)
+            .query(
+              `SELECT * FROM admin_user
+              WHERE 
+                  username = @username
+                  AND BINARY_CHECKSUM(password) = BINARY_CHECKSUM(@password)`
+            );
+          if (users.recordset.length === 0) {
+            const pool = await sqlConnect();
+            const users = await pool
+              .request()
+              .input("username", sql.VarChar, credentials?.username)
+              .input("password", sql.VarChar, credentials?.password)
+              .query(
+                `SELECT * FROM customer_user
+                WHERE 
+                    username = @username
+                    AND BINARY_CHECKSUM(password) = BINARY_CHECKSUM(@password)`
+              );
+            // const users: any = await executeQuery(query, []);
+            if (users.recordset.length === 0) {
+              return null;
+            } else {
+              user = {
+                user_account: users.recordset,
+                name: "customer",
+              };
+              if (!user) {
+                return null; // User not found
+              }
+              return user;
+            }
+          } else {
+            user = {
+              user_account: users.recordset,
+              name: "admin",
+            };
+            if (!user) {
+              return null; // User not found
+            }
+            return user;
+          }
+          return null;
+        } catch (error) {
+          console.error("Authentication error: ", error);
+          return new NextResponse("Database Error", { status: 500 });
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.user_account = user.user_account; // Adjust according to the structure of the user object you wish to persist in the token
+        token.name = user.name;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user_account = token.user_account; // Ensure the session reflects the structure you need on the client side
+        session.name = token.name;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/auth/signin",
+  },
+});
+
+export { handler as GET, handler as POST };
---
+++ src/app/types/next-auth.d.ts
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user_account: [
+      {
+        /** The user's postal address. */
+        id: string;
+        email: string;
+        status: string;
+        telephone: string;
+        username: string;
+        password: string;
+        name: string;
+        department: string;
+        initial_name: string;
+      }
+    ];
+    name: string;
+  }
+  interface User {
+    user_account: any;
+    name: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface User_Account {
+    email: string;
+    name: string;
+  }
+  interface JWT {
+    /** OpenID ID Token */
+    user_account?: any;
+    name: string;
+  }
+}
